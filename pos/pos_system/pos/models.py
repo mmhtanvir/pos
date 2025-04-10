39d5190807c923b403c6c1187e90d073ef6a9aca
Tanvir <82567711+mmhtanvir@users.noreply.github.com>
2025-04-10 11:50:11 +0600

    added sale type and payment method

--- pos/pos_system/pos/models.py
+++ pos/pos_system/pos/models.py
@@ -38,9 +38,21 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.full_name} ({self.phone})"
 
-class Sale(models.Model):    
+class Sale(models.Model):
+    SALE_TYPE_CHOICES = [
+        ('paid', 'Paid'),
+        ('unpaid', 'Unpaid')
+    ]
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Cash'),
+        ('online_payment', 'Bkash/Nagad/Upay'),
+        ('transfer', 'Bank Transfer'),
+    ]
+    
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     sale_date = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=15, choices=PAYMENT_METHOD_CHOICES, null=True, blank=True)
+    sale_type = models.CharField(max_length=7, choices=SALE_TYPE_CHOICES)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
