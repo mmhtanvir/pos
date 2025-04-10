07ff32bee0b928372a3a3386e29c8e44d6912288
Tanvir <82567711+mmhtanvir@users.noreply.github.com>
2025-04-10 11:40:18 +0600

    added customer and sale model

--- pos/pos_system/pos/models.py
+++ pos/pos_system/pos/models.py
@@ -30,3 +30,18 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.part_name} ({self.part_number})"
+
+class Customer(models.Model):
+    full_name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return f"{self.full_name} ({self.phone})"
+
+class Sale(models.Model):    
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    sale_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Sale #{self.id} - {self.customer.full_name}"   
